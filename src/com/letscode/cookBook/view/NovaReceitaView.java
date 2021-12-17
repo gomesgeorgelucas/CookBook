8a942767b7886c80a9903c9fae6598919b287bae
George Lucas Gomes <gomes.georgelucas@gmail.com>
2021-12-17 00:38:50 -0400

    Implementado Adicionar Ingredientes

--- src/com/letscode/cookBook/view/NovaReceitaView.java
+++ src/com/letscode/cookBook/view/NovaReceitaView.java
@@ -4,8 +4,10 @@ import com.letscode.cookBook.domain.Ingrediente;
 import com.letscode.cookBook.domain.Receita;
 import com.letscode.cookBook.domain.Rendimento;
 import com.letscode.cookBook.enums.Categoria;
+import com.letscode.cookBook.enums.TipoMedida;
 import com.letscode.cookBook.enums.TipoRendimento;
 
+import javax.sound.midi.Soundbank;
 import java.util.*;
 
 /**
@@ -22,8 +24,8 @@ public class NovaReceitaView {
     Categoria categoria;
     int tempoPreparo;
     Rendimento rendimento;
-    List<Ingrediente> ingredientes;
-    List<String> modoPreparo;
+    List<Ingrediente> ingredientes = new ArrayList<>();
+    List<String> modoPreparo = new ArrayList<>();
 
     public Receita nova() {
         this.receita = new Receita(askNome(), askCategoria(), askTempoPreparo(),
@@ -126,9 +128,57 @@ public class NovaReceitaView {
     }
 
     private List<Ingrediente> askIngredientes() {
-        System.out.println("Pede ingredientes");
-        List<Ingrediente> ingredientes = new ArrayList<>();
-        return ingredientes;
+        System.out.println("Inserir ingredientes.");
+
+        String nome = "";
+        double quantidade = 0;
+        int tipoMedida = 0;
+
+        String input = "";
+        do {
+            input = "";
+            do {
+                System.out.println("Nome do ingrediente: ");
+                nome = new Scanner(System.in).nextLine();
+                if (nome.isBlank()) {
+                    System.out.println("Nome inválido!");
+                }
+            } while (nome.isBlank());
+
+            try {
+                do {
+                    System.out.println("Qual é o tipo de medida do ingrediente da receita?");
+                    for (TipoMedida tipo : TipoMedida.values()) {
+                        System.out.printf("%d - %s%n", tipo.ordinal(), tipo.name());
+                    }
+                    tipoMedida = new Scanner(System.in).nextInt();
+                    if (tipoMedida < 0 || tipoMedida >= TipoMedida.values().length) {
+                        System.out.println("Tipo inválido!");
+                    }
+                } while (tipoMedida < 0 || tipoMedida >= TipoMedida.values().length);
+
+                do {
+                    System.out.println("Qual é a quantidade em/de " + TipoMedida.values()[tipoMedida].name() + "?");
+                    quantidade = new Scanner(System.in).nextDouble();
+                    if (quantidade <= 0) {
+                        System.out.println("Quantidade inválida!");
+                    }
+                } while(quantidade <= 0);
+
+                this.ingredientes.add(new Ingrediente(nome, quantidade, TipoMedida.values()[tipoMedida]));
+            } catch (InputMismatchException e) {
+                System.out.println("Ingrediente inválido!");
+                continue;
+            }
+
+            do {
+                System.out.print("Novo ingrediente?(S/N)");
+                input = new Scanner(System.in).next().toUpperCase();
+            } while (!input.equals("S") && !input.equals("N"));
+
+        } while(!input.equals("N"));
+
+        return this.ingredientes;
     }
 
     private List<String> askModoPreparo() {
